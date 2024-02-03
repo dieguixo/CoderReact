399cd381e3c658dda7f2beb28851e01b222582aa
Diego Mata <diegomata@hotmail.com>
2024-02-02 23:15:44 -0300

    color blanched almond

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,7 +1,5 @@
-import { useState } from "react"
 
 export const ItemListContainer = ({mensaje}) => {
-    const [cantidad, setCantidad] = useState (0)
     return (
         <div>
             <p className="mensaje">{mensaje}</p>
